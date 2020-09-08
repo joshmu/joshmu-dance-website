5e0959d0d83209d356ab71a2a9ffe0219e8de896
Josh Mu <mu@joshmu.com>
2020-09-08 16:55:06 +1000

    :zap: footer social icon animation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import {
   AiOutlineTwitter as TwitterIcon,
 } from 'react-icons/ai'
 import { FaFacebookF as FacebookIcon } from 'react-icons/fa'
+import Reveal from './Reveal'
 
 export default function Footer() {
   return (
@@ -17,30 +18,45 @@ export default function Footer() {
       </div>
       <div>
         <ul className='flex items-center justify-center'>
-          <li>
+          <Reveal
+            variants={{
+              hidden: { opacity: 0, x: -20 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <a
               className='inline-block px-1 py-4 transition-colors duration-300 ease-in-out cursor-pointer hover:text-themeAccent'
               href='https://www.facebook.com/josh.mu'
             >
               <FacebookIcon className='text-xl fill-current' />
             </a>
-          </li>
-          <li>
+          </Reveal>
+          <Reveal
+            variants={{
+              hidden: { opacity: 0, y: 20 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
             <a
               className='inline-block px-1 py-4 transition-colors duration-300 ease-in-out cursor-pointer hover:text-themeAccent'
               href='https://www.instagram.com/joshmu'
             >
               <InstagramIcon className='text-xl fill-current' />
             </a>
-          </li>
-          <li>
+          </Reveal>
+          <Reveal
+            variants={{
+              hidden: { opacity: 0, x: 20 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <a
               className='inline-block px-1 py-4 transition-colors duration-300 ease-in-out cursor-pointer hover:text-themeAccent'
               href='https://www.twitter.com/josh_mu_'
             >
               <TwitterIcon className='text-xl fill-current' />
             </a>
-          </li>
+          </Reveal>
         </ul>
       </div>
     </div>
