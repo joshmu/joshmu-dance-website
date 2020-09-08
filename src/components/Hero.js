5c6065dbdd6c82786f495c854aab6b576e3cbbb2
Josh Mu <mu@joshmu.com>
2020-09-08 18:03:50 +1000

    :zap: scroll overflow fixes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,28 +11,25 @@ export default function Hero() {
   const { ref } = useLocation('home')
 
   return (
-    <div
-      ref={ref}
-      className='relative w-full h-screen overflow-hidden text-themeBackground'
-    >
+    <div ref={ref} className='relative w-full h-screen text-themeBackground'>
       <FixedBackground
         src='./assets/waves.jpg'
         alt='josh upside down in the waves'
       >
         <Overlay />
-        <div className='relative z-20 flex flex-col items-center justify-center w-full h-full'>
+        <div className='relative flex flex-col items-center justify-center w-full h-full'>
           <h1
             onClick={toggleTheme}
-            className='font-semibold text-center uppercase cursor-pointer text-7xl sm:text-8xl'
+            className='z-10 font-semibold text-center uppercase cursor-pointer text-7xl sm:text-8xl'
           >
             josh mu
           </h1>
-          <p>
+          <p className='z-10'>
             <span>performer</span> | <span>choreographer</span> |{' '}
             <span>teacher</span>
           </p>
         </div>
-        <div className='absolute bottom-0 z-30 flex items-center justify-center w-full mb-8 text-4xl '>
+        <div className='absolute bottom-0 z-10 flex items-center justify-center w-full mb-8 text-4xl '>
           <ArrowDownIcon
             onClick={() => scrollToRef('about')}
             className='transition-colors duration-300 ease-in-out cursor-pointer fill-current animate-bounce hover:text-themeAccent'
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -22,7 +22,7 @@ export default function Layout({ children }) {
         <div
           className={`${
             theme === 'dark' ? 'theme-dark' : 'theme-light'
-          } text-themeText bg-themeBackground transition-colors duration-300 ease-in-out font-sans overflow-x-hidden`}
+          } text-themeText bg-themeBackground transition-colors duration-300 ease-in-out font-sans overflow-y-hidden`}
         >
           {/* content */}
           <Navbar />
