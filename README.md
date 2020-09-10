9fa2c4670220c002907308e9d28230e39ee856cc
Josh Mu <mu@joshmu.com>
2020-09-10 15:46:05 +1000

    :sparkles: zero added

--- README.md
+++ README.md
@@ -8,5 +8,10 @@ Feel free to get in touch or fly my some feedback on how I could improve this. â
 - [Tailwind CSS](https://tailwindcss.com/)
 - [Framer Motion](https://www.framer.com/motion/)
 - [react-intersection-observer](https://github.com/thebuilder/react-intersection-observer)
+- [react-icons](https://github.com/react-icons/react-icons)
+- [react-scroll](https://github.com/fisshy/react-scroll)
+- [react-device-detect](https://github.com/duskload/react-device-detect)
+- [react-ga](https://github.com/react-ga/react-ga)
+- [nodemailer](https://github.com/nodemailer/nodemailer)
 
 Some additional experimental features are enabled in `tailwind.config.js`: _uniformColorPalette, extendedSpacingScale, extendedFontSizeScale_
--- src/components/News.js
+++ src/components/News.js
@@ -6,28 +6,39 @@ export default function News(props) {
   const { ref } = useLocation('news')
 
   return (
-    <div ref={ref} className='container py-24 mx-auto' {...props}>
-      <h2 className='text-3xl font-light text-center uppercase'>
-        What I'm doing in <span className='font-semibold'>2020</span>
-      </h2>
-      <LineAccent center />
-      <div className='mt-8'>
-        <Reveal>
-          <p className='text-center text-themeTextSecondary'>hibernating. 😴</p>
-        </Reveal>
-      </div>
+    <>
+      <div ref={ref} className='container py-24 mx-auto' {...props}>
+        <h2 className='text-3xl font-light text-center uppercase'>
+          What I'm doing in <span className='font-semibold'>2020</span>
+        </h2>
+        <LineAccent center />
+        <div className='my-8'>
+          <Reveal>
+            <p className='text-center text-themeTextSecondary'>
+              Here is a preview of most recent creation...{' '}
+              <span className='text-xl tracking-tight'>ZERO</span>. 🖤
+            </p>
+          </Reveal>
+        </div>
 
-      <div className='flex items-center justify-center w-full mt-8'>
-        <iframe
-          // className='w-full h-96'
-          width='900'
-          height='400'
-          src='https://www.youtube-nocookie.com/embed/LK-DOKAX_RU'
-          frameBorder='0'
-          allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-          allowFullScreen
-        ></iframe>
+        {/* ZERO Preview Footage */}
+        <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
+          <iframe
+            src='https://player.vimeo.com/video/456437416?autoplay=1&loop=1&byline=0&portrait=0'
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+            }}
+            frameBorder='0'
+            allow='autoplay; fullscreen'
+            allowFullScreen
+          ></iframe>
+          <script src='https://player.vimeo.com/api/player.js'></script>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
